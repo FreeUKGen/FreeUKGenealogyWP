64482613a321a926055eaaba2a6385ab1e0cd6ce
TazHinkle <63527657+TazHinkle@users.noreply.github.com>
2021-02-15 16:18:24 -0700

    Add files via upload

---
+++ donate.php
@@ -0,0 +1,8 @@
+<div id="center">
+  <a href="/test/freeGen/bmd" class="freeGen" id="donate">
+    <span class="btn-donate" id="donateOverlay"><?php include($_SERVER['DOCUMENT_ROOT'] . './svg/donateOverlay.svg');?></span>
+    <span class="btn-donate" id="donateCoin"><?php include($_SERVER['DOCUMENT_ROOT'] . './svg/donateCoin.svg');?></span>
+    <span class="btn-donate" id="donateBox"><?php include($_SERVER['DOCUMENT_ROOT'] . './svg/donateBox.svg');?></span>
+    <span class="btn-donate" id="donateBtn"><?php include($_SERVER['DOCUMENT_ROOT'] . './svg/donateBtn.svg');?>Donate</span>
+  </a>
+</div>
